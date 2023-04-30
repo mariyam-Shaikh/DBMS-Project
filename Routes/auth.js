ab8e2d2e5490ba611784bfa4a02c5a50c91b2a5f
Aliza-Ahsan <121791709+Aliza-Ahsan@users.noreply.github.com>
2023-04-30 13:25:08 +0530

    Update auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/public', authController.register);
 router.post('/public/ticket', authController.ticketform);
 router.post('/public/login', authController.login);
-
+router.post('/public/shop', authController.shop);
 
 module.exports = router;
